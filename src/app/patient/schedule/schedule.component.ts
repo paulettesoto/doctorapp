383a63d847d620af484ab538acba36010e0eb9d8
erandizs <damariserandizavalagmail.com>
2023-11-26 15:41:34 -0700

    cuarto commit

--- src/app/patient/schedule/schedule.component.ts
+++ src/app/patient/schedule/schedule.component.ts
@@ -6,5 +6,33 @@ import { Component } from '@angular/core';
   styleUrls: ['./schedule.component.css']
 })
 export class ScheduleComponent {
+  date: string;
+  doctorname: string;
+  patient: string;
+  treatment: string;
+  doctoraddress: string;
+  doctorspecialty: string;
+  hourdate: string;
+ 
 
+  constructor() {
+    this.date = '';
+    this.doctorname = '';
+    this.patient = '';
+    this.treatment = '';
+    this.doctoraddress = '';
+    this.doctorspecialty = '';
+    this.hourdate = '';
+   
+  }
+
+  agendar() {
+    console.log(this.doctorname);
+    console.log(this.date);
+    console.log(this.patient);
+    console.log(this.treatment);
+    console.log(this.doctoraddress);
+    console.log(this.doctorspecialty); 
+    console.log(this.hourdate);
+  }
 }
