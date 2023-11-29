b550a4b58087bdc9385a68ffacf27fefe82265d1
erandizs <damariserandizavalagmail.com>
2023-11-29 16:15:54 -0700

    29nov

--- src/app/doctor-schedule/new-appointment/new-appointment.component.ts
+++ src/app/doctor-schedule/new-appointment/new-appointment.component.ts
@@ -6,5 +6,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./new-appointment.component.css']
 })
 export class NewAppointmentComponent {
+  name: string;
+  lastname: string;
+  lastname2: string;
+  phonenumber: string;
+  age: string;
+  email: string;
+  date:string;
+treatment:string;
 
+  constructor() {
+    this.name = '';
+    this.lastname = '';
+    this.lastname2 = '';
+    this.phonenumber = '';
+    this.age = '';
+    this.email = '';
+    this.date='';
+    this.treatment='';
+  }
+
+  agendar() {
+    console.log(this.email);
+  
+  }
 }
