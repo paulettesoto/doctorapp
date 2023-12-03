7c7047e36d674536d9af1fdc6675da612eeeb62c
paulette <esmesotoangeles863@gmail.com>
2023-12-03 01:17:50 -0700

    3/12

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,24 +1,52 @@
 import { Component } from '@angular/core';
-
+import { FormGroup, FormBuilder } from '@angular/forms';
+import { Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
+
 export class LoginComponent {
   user: string;
   password: string;
   type: string;
 
-  constructor() {
+  constructor(private http:HttpClient, private route:Router) {
     this.user = '';
     this.password = '';
     this.type = '';
   }
 
+
   login() {
     console.log(this.user);
     console.log(this.password);
     console.log(this.type);
+    //const spanElement: HTMLElement | null = document.getElementById('error')!;
+    const url = `https://doctorappbackend-wpqd.onrender.com/login?user=${this.user}&pswrd=${this.password}`;
+    this.http.get(url).subscribe(
+      (response: any) => {
+        // Manejar la respuesta aquí
+        console.log(response); // Ver la respuesta en la consola
+        const usr = response;
+        if(typeof usr === 'number'){
+          localStorage.setItem("user",usr.toString());
+          this.route.navigate(['/schedule/scheduleview']);
+
+        }else {
+         // spanElement.textContent = 'Usuario o contraseña incorrectos';
+        }
+
+        
+      },
+      (error) => {
+        //spanElement.textContent = 'Error de sistema';
+        console.error('Error al obtener los datos:', error);
+      }
+    );
   }
+
+  
 }
