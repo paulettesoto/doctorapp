79af3738374ec958cfdab1054420d1d3051caed6
erandizs <damariserandizavalagmail.com>
2023-12-09 18:06:34 -0700

    tabla de respuestas en bd, tabla en html, boton de imprimir

--- src/app/patients/patient-detail/patient-detail.component.ts
+++ src/app/patients/patient-detail/patient-detail.component.ts
@@ -1,11 +1,13 @@
-import { Component } from '@angular/core';
-
+import { Component , OnInit} from '@angular/core';
+import { storageService } from 'src/app/storage.service';
+import { HttpClient, HttpHeaders, HttpParams} from '@angular/common/http';
 @Component({
   selector: 'app-patient-detail',
   templateUrl: './patient-detail.component.html',
   styleUrls: ['./patient-detail.component.css']
 })
 export class PatientDetailComponent {
+  resps: any[] = [];
   name: string;
   lastname: string;
   lastname2: string;
@@ -26,5 +28,8 @@ export class PatientDetailComponent {
 
   subir() {
  
+  }
+  descargar(){
+    
   }
 }
