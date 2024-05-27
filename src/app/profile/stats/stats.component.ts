38e75cc2b1ddc79acd2cb65b48c95d7750e3441e
danielniebla <82241926+danielniebla@users.noreply.github.com>
2024-05-27 15:10:15 -0700

    Daniel (#12)
    * x
    
    * correcting the double register error
    
    disable the buttons after a click, put the cursor in await mode, then when something happen turn it  to dafault.
    
    * vistas
    
    boton regresar, estrellas y dias/horarios
    
    * vistas
    
    nueva cita paciente
    
    * validaciones
    
    todas las validaciones que se ocupaban
    
    * tablas
    
    tablas para lo de herman

--- src/app/profile/stats/stats.component.ts
+++ src/app/profile/stats/stats.component.ts
@@ -1,71 +1,236 @@
 import { Component, ViewChild } from '@angular/core';
 import {
-  ApexAxisChartSeries,
+  ApexNonAxisChartSeries,
+  ApexResponsive,
   ApexChart,
-  ChartComponent,
-  ApexDataLabels,
-  ApexPlotOptions,
-  ApexYAxis,
-  ApexTitleSubtitle,
+  ApexStroke,
+  ApexGrid,
+  ApexAxisChartSeries,
   ApexXAxis,
-  ApexFill
+  ApexDataLabels,
+  ApexLegend,
+  ApexPlotOptions,
+  ApexTitleSubtitle,
+  ChartComponent,
+  ApexFill,
+  ApexYAxis
 } from "ng-apexcharts";
 
+
+export type schedulesOptions = {
+  series: ApexAxisChartSeries;
+  chart: ApexChart;
+  dataLabels: ApexDataLabels;
+  plotOptions: ApexPlotOptions;
+  xaxis: ApexXAxis;
+  yaxis: ApexYAxis;
+  colors: string[];
+  fill: ApexFill;
+  legend: ApexLegend;
+  title: ApexTitleSubtitle;
+  grid: ApexGrid;
+};
 export type ChartOptions = {
   series: ApexAxisChartSeries;
   chart: ApexChart;
   dataLabels: ApexDataLabels;
   plotOptions: ApexPlotOptions;
-  yaxis: ApexYAxis;
+  responsive: ApexResponsive[];
   xaxis: ApexXAxis;
+  legend: ApexLegend;
   fill: ApexFill;
+};
+export type DonaOptions = {
+  series: ApexNonAxisChartSeries;
+  chart: ApexChart;
+  responsive: ApexResponsive[];
+  labels: any;
+};
+export type AgesOptions = {
+  series: ApexAxisChartSeries;
+  chart: ApexChart;
+  dataLabels: ApexDataLabels;
+  plotOptions: ApexPlotOptions;
+  xaxis: ApexXAxis;
+  colors: string[];
+  legend: ApexLegend;
   title: ApexTitleSubtitle;
 };
-
+export type patientsOptions = {
+  series: ApexAxisChartSeries;
+  chart: ApexChart;
+  xaxis: ApexXAxis;
+  dataLabels: ApexDataLabels;
+  grid: ApexGrid;
+  stroke: ApexStroke;
+  title: ApexTitleSubtitle;
+};
 @Component({
   selector: 'app-stats',
   templateUrl: './stats.component.html',
   styleUrls: ['./stats.component.css']
 })
 export class StatsComponent {
-  @ViewChild("chart") chart!: ChartComponent;
-  public chartOptions: ChartOptions;
-  @ViewChild("chart2") chart2!: ChartComponent;
-  public chartOptions2: ChartOptions;
-
+  @ViewChild("star") star!: ChartComponent;
+  public starOptions: DonaOptions;
+  @ViewChild("ocupation") ocupation!: ChartComponent;
+  public ocupationOptions: DonaOptions;
+  @ViewChild("tartment") tratment!: ChartComponent;
+  public TratmentsOptions: DonaOptions;
+  @ViewChild("ages") ages!: ChartComponent;
+  public agesOptions: AgesOptions;
+  @ViewChild("patients") patients!: ChartComponent;
+  public patientsOptions: patientsOptions;
+  @ViewChild("cancel") cancel!: ChartComponent;
+  public cancelOptions: ChartOptions;
+  @ViewChild("schedules") schedules!: ChartComponent;
+  public schedulesOptions: schedulesOptions;
+  
   constructor() {
-    this.chartOptions = {
+    this.starOptions = {
+      series: [44, 55, 13, 43, 22],//---aqui asignas las estrellas en orden--------------------------------------------------------
+      chart: {
+        type: "donut"
+      },
+      labels: ["5 Estrellas", "4 Estrellas", "3 Estrellas", "2 Estrellas", "1 Estrellas"],
+      responsive: [
+        {
+          breakpoint: 480,
+          options: {
+            chart: {
+              width: 200
+            },
+            legend: {
+              position: "bottom"
+            }
+          }
+        }
+      ]
+    };
+    this.TratmentsOptions = {
+      series: [50, 30, 15, 25, 35],//---aqui vas a recorrer las values----------------------------------------------------------------
+      chart: {
+        type: "donut"
+      },
+      labels: ["lavado", "puntos", "cocas", "tes", "maria "],//aqui vas a recorrer las keys----------------------------------------------------------------
+      responsive: [
+        {
+          breakpoint: 480,
+          options: {
+            chart: {
+              width: 200
+            },
+            legend: {
+              position: "bottom"
+            }
+          }
+        }
+      ]
+    };
+    this.ocupationOptions = {
+      series: [44, 55, 13, 43, 22],//---aqui asignas las estrellas en orden--------------------------------------------------------
+      chart: {
+        type: "donut"
+      },
+      labels: ["5 Estrellas", "4 Estrellas", "3 Estrellas", "2 Estrellas", "1 Estrellas"],
+      responsive: [
+        {
+          breakpoint: 480,
+          options: {
+            chart: {
+              width: 200
+            },
+            legend: {
+              position: "bottom"
+            }
+          }
+        }
+      ]
+    };
+    this.agesOptions = {
       series: [
         {
-          name: "Inflation",
-          data: [2.3, 3.1, 4.0, 10.1, 4.0, 3.6, 3.2, 2.3, 1.4, 0.8, 0.5, 0.2]
+          name: "",
+          data: [200, 330, 548, 740, 880, 990, 1100, 1380]//aqui van los datos acomodados por rangos de mayor a menor---------------------------
+        }
+      ],
+      chart: {
+        type: "bar",
+        height: 350
+      },
+      plotOptions: {
+        bar: {
+          borderRadius: 0,
+          horizontal: true,
+          distributed: true,
+          barHeight: "80%",
+          isFunnel: true
+        }
+      },
+      colors: [
+        "#F44F5E",
+        "#E55A89",
+        "#D863B1",
+        "#CA6CD8",
+        "#B57BED",
+        "#8D95EB",
+        "#62ACEA",
+        "#4BC3E6"
+      ],
+      dataLabels: {
+        enabled: true,
+        formatter: function (val, opt) {
+          return opt.w.globals.labels[opt.dataPointIndex];
+        },
+        dropShadow: {
+          enabled: true
+        }
+      },
+      title: {
+        text: "Pyramid Chart",
+        align: "center"
+      },
+      xaxis: {
+        categories: [
+          "mas de 70 años", "60 - 70 años", "50 - 60 años", "40 - 50 años", "30 - 40 años", "20 - 30 años", "10 - 20 años", "0 - 10 años"
+        ]
+      },
+      legend: {
+        show: false
+      }
+    };
+    this.patientsOptions = {
+      series: [
+        {
+          name: "Atendidos",
+          data: [10, 41, 35, 51, 49, 62, 69, 91, 148]// pacientes atendidos en ese dia--------------------------------------------------------
         }
       ],
       chart: {
         height: 350,
-        type: "bar"
-      },
-      plotOptions: {
-        bar: {
-          dataLabels: {
-            position: "top" // top, center, bottom
-          }
+        type: "line",
+        zoom: {
+          enabled: false
         }
       },
       dataLabels: {
-        enabled: true,
-        formatter: function(val) {
-          return val + "%";
-        },
-        offsetY: -20,
-        style: {
-          fontSize: "12px",
-          colors: ["#304758"]
+        enabled: false
+      },
+      stroke: {
+        curve: "straight"
+      },
+      title: {
+        text: "Product Trends by Month",
+        align: "left"
+      },
+      grid: {
+        row: {
+          colors: ["#f3f3f3", "transparent"], // takes an array which will be repeated on columns
+          opacity: 0.5
         }
       },
-
       xaxis: {
-        categories: [
+        categories: [//dias--------------------------------------------------------
           "Jan",
           "Feb",
           "Mar",
@@ -74,185 +239,179 @@ export class StatsComponent {
           "Jun",
           "Jul",
           "Aug",
-          "Sep",
-          "Oct",
-          "Nov",
-          "Dec"
-        ],
-        position: "top",
-        labels: {
-          offsetY: -18
-        },
-        axisBorder: {
-          show: false
-        },
-        axisTicks: {
-          show: false
-        },
-        crosshairs: {
-          fill: {
-            type: "gradient",
-            gradient: {
-              colorFrom: "#D8E3F0",
-              colorTo: "#BED1E6",
-              stops: [0, 100],
-              opacityFrom: 0.4,
-              opacityTo: 0.5
-            }
-          }
-        },
-        tooltip: {
-          enabled: true,
-          offsetY: -35
-        }
-      },
-      fill: {
-        type: "gradient",
-        gradient: {
-          shade: "light",
-          type: "horizontal",
-          shadeIntensity: 0.25,
-          gradientToColors: undefined,
-          inverseColors: true,
-          opacityFrom: 1,
-          opacityTo: 1,
-          stops: [50, 0, 100, 100]
-        }
-      },
-      yaxis: {
-        axisBorder: {
-          show: false
-        },
-        axisTicks: {
-          show: false
-        },
-        labels: {
-          show: false,
-          formatter: function(val) {
-            return val + "%";
-          }
-        }
-      },
-      title: {
-        text: "Monthly Inflation in Argentina, 2002",
-        floating: false,
-        offsetY: 320,
-        align: "center",
-        style: {
-          color: "#444"
-        }
+          "Sep"
+        ]
       }
     };
-
-    // Opciones para el segundo gráfico
-    this.chartOptions2 = {
+    this.cancelOptions = {
       series: [
         {
-          name: "Inflation",
-          data: [2.3, 3.1, 4.0, 10.1, 4.0, 3.6, 3.2, 2.3, 1.4, 0.8, 0.5, 0.2]
+          name: "PRODUCT A",
+          data: [44, 55, 41, 67, 22, 43]
+        },
+        {
+          name: "PRODUCT B",
+          data: [13, 23, 20, 8, 13, 27]
+        },
+        {
+          name: "PRODUCT C",
+          data: [11, 17, 15, 15, 21, 14]
+        },
+        {
+          name: "PRODUCT D",
+          data: [21, 7, 25, 13, 22, 8]
+        }
+      ],
+      chart: {
+        type: "bar",
+        height: 350,
+        stacked: true,
+        toolbar: {
+          show: true
+        },
+        zoom: {
+          enabled: true
+        }
+      },
+      responsive: [
+        {
+          breakpoint: 480,
+          options: {
+            legend: {
+              position: "bottom",
+              offsetX: -10,
+              offsetY: 0
+            }
+          }
+        }
+      ],
+      plotOptions: {
+        bar: {
+          horizontal: false
+        }
+      },
+      xaxis: {
+        type: "category",
+        categories: [
+          "01/2011",
+          "02/2011",
+          "03/2011",
+          "04/2011",
+          "05/2011",
+          "06/2011"
+        ]
+      },
+      legend: {
+        position: "right",
+        offsetY: 40
+      },
+      fill: {
+        opacity: 1
+      },
+      dataLabels: {
+        enabled: true
+      }
+    };
+    this.schedulesOptions = {
+      series: [
+        {
+          data: [
+            {
+              x: "2008",
+              y: [2800, 4500]
+            },
+            {
+              x: "2009",
+              y: [3200, 4100]
+            },
+            {
+              x: "2010",
+              y: [2950, 7800]
+            },
+            {
+              x: "2011",
+              y: [3000, 4600]
+            },
+            {
+              x: "2012",
+              y: [3500, 4100]
+            },
+            {
+              x: "2013",
+              y: [4500, 6500]
+            },
+            {
+              x: "2014",
+              y: [4100, 5600]
+            }
+          ]
         }
       ],
       chart: {
         height: 350,
-        type: "bar"
+        type: "rangeBar",
+        zoom: {
+          enabled: false
+        }
       },
       plotOptions: {
         bar: {
-          dataLabels: {
-            position: "top" // top, center, bottom
-          }
+          isDumbbell: true,
+          columnWidth: 3,
+          dumbbellColors: [["#008FFB", "#00E396"]]
         }
       },
-      dataLabels: {
-        enabled: true,
-        formatter: function(val) {
-          return val + "%";
-        },
-        offsetY: -20,
-        style: {
-          fontSize: "12px",
-          colors: ["#304758"]
-        }
-      },
-
-      xaxis: {
-        categories: [
-          "Jan",
-          "Feb",
-          "Mar",
-          "Apr",
-          "May",
-          "Jun",
-          "Jul",
-          "Aug",
-          "Sep",
-          "Oct",
-          "Nov",
-          "Dec"
-        ],
+      legend: {
+        show: true,
+        showForSingleSeries: true,
         position: "top",
-        labels: {
-          offsetY: -18
-        },
-        axisBorder: {
-          show: false
-        },
-        axisTicks: {
-          show: false
-        },
-        crosshairs: {
-          fill: {
-            type: "gradient",
-            gradient: {
-              colorFrom: "#D8E3F0",
-              colorTo: "#BED1E6",
-              stops: [0, 100],
-              opacityFrom: 0.4,
-              opacityTo: 0.5
-            }
-          }
-        },
-        tooltip: {
-          enabled: true,
-          offsetY: -35
-        }
+        horizontalAlign: "left",
+        customLegendItems: ["Product A", "Product B"]
       },
       fill: {
         type: "gradient",
         gradient: {
-          shade: "light",
-          type: "horizontal",
-          shadeIntensity: 0.25,
-          gradientToColors: undefined,
+          type: "vertical",
+          gradientToColors: ["#00E396"],
           inverseColors: true,
-          opacityFrom: 1,
-          opacityTo: 1,
-          stops: [50, 0, 100, 100]
+          stops: [0, 100]
         }
       },
-      yaxis: {
-        axisBorder: {
-          show: false
+      grid: {
+        xaxis: {
+          lines: {
+            show: true
+          }
         },
-        axisTicks: {
-          show: false
-        },
-        labels: {
-          show: false,
-          formatter: function(val) {
-            return val + "%";
+        yaxis: {
+          lines: {
+            show: false
           }
         }
       },
-      title: {
-        text: "Monthly Inflation in Argentina, 2002",
-        floating: false,
-        offsetY: 320,
-        align: "center",
+      xaxis: {
+        tickPlacement: "on"
+      },
+      dataLabels: {
+        enabled: true,
+        formatter: function(val: number[], opts: any) {
+          return `${val[0]} - ${val[1]}`;
+        },
         style: {
-          color: "#444"
+          colors: ['#F3F4F5', '#fff']
         }
+      },
+      yaxis: {
+        title: {
+          text: "Value Range"
+        }
+      },
+      colors: ["#008FFB", "#00E396"],
+      title: {
+        text: "Yearly Product Ranges",
+        align: "center"
       }
     };
+    
   }
 }
